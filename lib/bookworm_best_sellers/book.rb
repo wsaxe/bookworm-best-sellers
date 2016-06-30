44f954195280e7d68ea3f0267c1c236d6bbf55f8
wsaxe <geosaxe@gmail.com>
2016-06-29 23:59:49 -0500

    Completed author scrape method

--- lib/bookworm_best_sellers/book.rb
+++ lib/bookworm_best_sellers/book.rb
@@ -5,34 +5,32 @@ class BookwormBestSellers::Book
     self.scrape_books
   end
 
-
   def self.scrape_books
     books = []
-    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/the-new-york-times-bestsellers-hardcover-nonfiction/_/N-1p5q"))
-    title_array = doc.search("p.product-info-title").text.split("\n").reject { |item| item.nil? || item == "" }.map { |ind_title| ind_title[0...-13] }
-    title_array.each do |ind_title|
+    self.scrape_titles.each do |ind_title|
       book = self.new
       book.title = ind_title
       books << book
     end
-    books.each do |ind_book|
-      ind_book.author = "John Smith"
+    books.each.with_index(0) do |ind_book, i|
+      ind_book.author = self.scrape_authors[i]
     end
     books
   end
 
-  def self.scrape_console
+  def self.scrape_titles
     doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/the-new-york-times-bestsellers-hardcover-nonfiction/_/N-1p5q"))
-    author_array = doc.search("span.contributor").to_s.split("/a")
-    author_array.map! do |jumble|
-      jumble[-25..-1]
+    title_array = doc.search("p.product-info-title").text.split("\n").reject { |item| item.nil? || item == "" }.map { |ind_title| ind_title[0...-13] }
+    title_array
+  end
+
+  def self.scrape_authors
+    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/the-new-york-times-bestsellers-hardcover-nonfiction/_/N-1p5q"))
+    author_array = doc.search("span.contributor").to_s.split("/a").map! { |author| author[-25..-1] }.reject { |item| item.nil? || item == "" }
+    author_array.map! do |pile|
+      pile.split(">")[1]
     end
-    #clean_array = []
-    #author_array.each do |author|
-      #clean_author = author.text
-      #clean_array << clean_author
-    #end
-    #clean_array
+    author_array.map! { |ind_author| ind_author[0...-1] }
     author_array
   end
 
